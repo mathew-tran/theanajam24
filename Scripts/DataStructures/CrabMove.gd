f13bd7118f9833ebdcbb20f0663198293b4dc675
mathew-tran <mathew.tran.dev@gmail.com>
2024-07-01 06:29:35 -0700

    start up project

---
+++ Scripts/DataStructures/CrabMove.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name CrabMove
+
+@export var MoveName : String
+@export var Cost : int
+
+func PerformMove():
+	pass
+
---
+++ Scripts/DataStructures/CrabMoveAttack.gd
@@ -0,0 +1,6 @@
+extends CrabMove
+
+@export var Damage : int
+
+func PerformMove():
+	pass
