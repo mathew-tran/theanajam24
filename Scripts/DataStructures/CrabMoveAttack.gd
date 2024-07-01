54652467f998eea83a7a3d1b1c873794e52dcdd3
mathew-tran <mathew.tran.dev@gmail.com>
2024-07-01 12:36:48 -0700

    add accuracy and crit chance to attacks

--- Scripts/DataStructures/CrabMoveAttack.gd
+++ Scripts/DataStructures/CrabMoveAttack.gd
@@ -1,7 +1,15 @@
 extends CrabMove
 
 @export var Damage : int
+@export var DamageBonus : int
+@export var CritChance : int
 
 func PerformMove(target : CrabUnit):
 	print(target.Name + "has been attacked")
-	target.Health.TakeDamage(Damage)
+	var damage = Damage + randi() % (DamageBonus + 1)
+	var result = randi() % 100
+	if result < CritChance:
+		damage *= 1.5
+		damage = floor(damage)
+		print("CRIT")
+	target.Health.TakeDamage(damage)
