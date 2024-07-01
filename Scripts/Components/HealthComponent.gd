431213ab1d4fc48f15a88959ac8e18b8cf4cf837
mathew-tran <mathew.tran.dev@gmail.com>
2024-07-01 06:38:47 -0700

    working on crab unit

---
+++ Scripts/Components/HealthComponent.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var Amount : int
+
+var MaxHealth = 0
+
+func Setup():
+	MaxHealth = Amount
+
+func IsAlive():
+	return Amount > 0
+
+func TakeDamage(amount):
+	Amount -= amount
+
+func Heal(amount):
+	Amount += amount
+	if Amount < 0:
+		Amount = 0
